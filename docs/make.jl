74e6a4f72fb70a98ff9097ca3830919ef66c6090
Haris Orgn <organtzh@gmail.com>
2024-08-29 16:10:35 +0300

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,6 @@
 using Neuroblox
 using Documenter
 
-cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
-cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
-
 DocMeta.setdocmeta!(Neuroblox, :DocTestSetup, :(using Neuroblox); recursive = true)
 
 include("pages.jl")
@@ -11,13 +8,12 @@ include("pages.jl")
 makedocs(sitename = "Neuroblox",
     authors = "Neuroblox Inc.",
     modules = [Neuroblox],
-    clean = true, doctest = false, linkcheck = true,
+    clean = true, doctest = false, linkcheck = false,
     warnonly = [:docs_block, :missing_docs],
     format = Documenter.HTML(assets = ["assets/favicon.ico"]),
-        #canonical = "https://docs.sciml.ai/LinearSolve/stable/"),
     pages = pages)
 
-repo =  "github.com/Neuroblox/NeurobloxDocsHost"
+repo =  "github.com/Neuroblox/NeurobloxDocsHost.git"
 
 withenv("GITHUB_REPOSITORY" => repo) do
     deploydocs(; repo = repo, push_preview = true)
