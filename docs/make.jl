aacd754541ce0138f52ffda7c3d39ae1c42e8e24
Haris Orgn <organtzh@gmail.com>
2024-08-30 14:13:20 +0300

    enable linkcheck again

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ include("pages.jl")
 makedocs(sitename = "Neuroblox",
     authors = "Neuroblox Inc.",
     modules = [Neuroblox],
-    clean = true, doctest = false, linkcheck = false,
+    clean = true, doctest = false, linkcheck = true,
     warnonly = [:docs_block, :missing_docs],
     format = Documenter.HTML(assets = ["assets/favicon.ico"]),
     pages = pages)
