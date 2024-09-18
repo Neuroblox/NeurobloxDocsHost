f67b1b259736f252f9d794705f802e1215ce5c52
Helmut Strey <Helmut.Strey@stonybrook.edu>
2024-09-18 16:03:31 -0400

    animated QIF neuron

--- docs/src/tutorials/QIF_movie.jl
+++ docs/src/tutorials/QIF_movie.jl
@@ -16,20 +16,12 @@ length(unknowns(sys))
 prob = ODEProblem(sys, [], (0.0, 1000), [])
 sol = solve(prob, Vern7(), saveat=0.1)
 plot(sol.t,sol[1,:],xlims=(0,1000),xlabel="time (ms)",ylabel="mV")
-p_old = prob.p[1]
-p_old[10] = 0.5 # I_in
-prob2 = remake(prob, p = p_old)
-sol = solve(prob2, Vern7(), saveat=0.1)
+pI_in = parameters(sys)[findall(x->contains(string(x),"I_in"),parameters(sys))][1]
+
 I = 0:0.01:0.1
 anim = @animate for i in I
     @show i
-    qif1 = QIFNeuron(name=Symbol("qif1"), I_in=i)
-    g = MetaDiGraph()
-    add_blox!.(Ref(g), [qif1])
-
-    @named sys = system_from_graph(g)
-    sys = structural_simplify(sys)
-    prob_new = ODEProblem(sys, [], (0.0, 1000), [])
+    prob_new = remake(prob,p=[pI_in => i])
     sol_new = solve(prob_new, Vern7(), saveat=0.1)
     plot_qif = plot(sol_new.t,sol_new[1,:],xlims=(0,1000),xlabel="time (ms)",ylabel="mV",label="I_in=$(i)",legend=:top)
 end
