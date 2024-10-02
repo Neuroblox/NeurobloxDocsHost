778742281bfe6c54762262ce6db9869595dfb866
João Rafael Silva de Azeredo <j288818@dac.unicamp.br>
2024-10-01 21:23:44 -0300

    Add comments to João's tutorials

--- docs/src/tutorials/basic_izhikevich_neuron.jl
+++ docs/src/tutorials/basic_izhikevich_neuron.jl
@@ -1,3 +1,26 @@
+# How to create an ODE system and model an Izhikevich Neuron
+
+# The Izhikevich model
+
+# The Izhikevich model is described by the following two-dimensional system of ordinary differential equations:
+
+# ```math
+# \begin{align}
+# \frac{dv}{dt} &= 0.04 v^2 + 5 v + 140 - u + I \\
+# \frac{du}{dt} &= a(bv - u)
+# \end{align}
+# ```
+
+# with the auxiliary after-spike resetting
+
+# ```math
+# \begin{align}
+# \text{if }v \geq 30\text{ mV, then }\begin{cases}v\leftarrow c, u\leftarrow u + d.\end{cases}
+# \end{align}
+# ```
+
+# In order to create this model in Julia, we first need to create an ODE system. We can do this by basically replicating what we do to a single equation, but adding a few more as below:
+
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using DifferentialEquations
 
@@ -7,11 +30,23 @@ using DifferentialEquations
 equation = [D(v) ~ 0.04 * v ^ 2 + 5 * v + 140 - u + I
            D(u) ~ a * (b * v - u)]
 
+# Now, we need to create the event that generates the spikes. For this, we create a function continuous event to represent the discontinuity in our functions. Those should be written in the following format:
+
+name_of_event = [[event_1] => [effect_1]
+                [event_2] => [effect_2]
+                ...
+                [event_n] => [effect_n]]
+
+# In our case, it'll be:
 
 event = [[v ~ 30.0] => [u ~ u + d]
         [v ~ 30.0] => [v ~ c]]
 
+# and now, using the `ODESystem` function, we'll create our system of ODE's. We need to declare our independent variable, dependent variables and parameters when creating this system, and if it exists (in this case, it does exist), we also need to declare our events:
+
 @named izh_system = ODESystem(equation, t, [v, u], [a, b, c, d, I]; continuous_events = event)
+
+# Finally, we define our parameters and solve our system:
 
 # Those below can change; I'm using the parameters for a chattering dynamic
 p = [a =>  0.02, b => 0.2, c => -50.0, d => 2.0, I => 10.0]
@@ -22,6 +57,8 @@ tspan = (0.0, 100.0)
 
 izh_prob = ODEProblem(complete(izh_system), u0, tspan, p)
 izh_sol = solve(izh_prob)
+
+# Now that our problem is solved, we just have to plot it:
 
 using CairoMakie
 
@@ -36,3 +73,5 @@ ax.title = "Izhikevich Neuron Model"
 axislegend(ax)
 
 display(fig)
+
+# We're done! You've just plotted an Izhikevich neuron.
--- docs/src/tutorials/beginner_tutorial.jl
+++ docs/src/tutorials/beginner_tutorial.jl
@@ -1,6 +1,14 @@
-# Basic Julia commands #
+# [Julia Basics and how to model an ODE](@id beginner_tutorial)
+
+# This initial tutorial will introduce Julia's basic structures and how to model an ordinary differential equation.
+
+# Julia's basic structures
+
+# In Julia, we assign a variable simply by calling it by its name and assigning a value to it using the `=` symbol:
 
 my_variable = 42
+
+# For basic math operations, we can just use the following operators:
 
 sum = 3 + 2
 product = 3 * 2
@@ -9,17 +17,21 @@ quotient = 3 / 2
 power = 3 ^ 2 
 modulus = 3 % 2
 
+# To define a function in Julia, we first write `function` followed by the function's name, its parameters, the commands and `end`:
+
 function tutorial(parameter)
     # Your code goes here
     println("The input was $parameter.")
 end
 
+# And we can simply call it by its name:
+ 
 tutorial("Neuroblox")
 # This should print "The input was Neuroblox."
 
-# ---------------------------------------------- #
+# Plotting graphs
 
-# CairoMakie basic Tutorial #
+# We can plot graphs in Julia using the `CairoMakie.jl` package. To add this package we use the terminal:
 
 using Pkg
 Pkg.add("CairoMakie")
@@ -28,9 +40,17 @@ Pkg.add("CairoMakie")
 using CairoMakie
 # This will load the package
 
+# Notice that `CairoMakie` is only one of Makie's backends for rendering. There are a few other such as `GLMakie`, but for this tutorial, we're going to use this one. After loading the package, we need an interval and a data set to generate a graph. We'll create some random data for now:
+
+using CairoMakie
+
 # Create some data (an interval and a random 10-element vector)
 x = 1:10    # This sets an interval
 y = rand(10)    # This gives us random y-values
+
+# To create the plot, we'll use the `lines` function. It can receive several inputs as arguments such as `color`, `linewidth`, `markersize` and many other. You can check all of them by yourself in Makie's official documentation webpage: https://docs.makie.org/stable/.
+
+# The said function returns us a tuple containing the overall figure containing the plot, the axis within the figure and the actual line plot created. To plot the data created, we can simply do:
 
 # Create the line plot with a few properties
 fig, ax, plt = lines(x, y, color:=black, label="Random Data")
@@ -45,9 +65,17 @@ save("line_plot.pdf", fig)
 # Display the plot
 display(fig)
 
-# ---------------------------------------------- #
+# You can try running the code above in your Julia terminal using your own data to see how it works.
 
-# EDO solving Tutorial #
+# Solving differential equations
+
+# An essential package we'll be using is `DifferentialEquations.jl`, a library designed to solve various types of differential equations, such as ODEs, SDEs and PDEs.
+
+# To use the `DifferentialEquations` library, we need to define a differential equation as a function, solve it and then plot it (we know how to do the last part).
+
+# Another useful library we'll be using is `ModelingToolkit.jl`, a Julia package designed to make our lives easiear when dealing with complex models and performing tasks such as model simplification, parameter estimation and code generation.
+
+# Let's solve the ODE $\frac{du}{dt}=-a \cdot u$  using this package. First we need to define what are our variables and what are our parameters. For this, we'll use the `@variables`and `@parameters` macros from `ModelingToolkit`:
 
 # Install and import these packages
 using Pkg
@@ -62,16 +90,24 @@ using DifferentialEquations
 # Define the parameter a
 @parameters a
 
+# Also, when using `ModelingToolkit`, the `~` symbol is used as the equal sign when writing an equation:
+
 # Define the differential equation du/dt = -a * u
 equation = D(u) ~ -a * u
 
+# Now, in order to build the ODE system, we use the `@named` macro to name the system and the `ODESystem` function:
+
 # Build the ODE system and assign it a name
 @named system = ODESystem(equation, t)
+
+# We also have to define our system's initial conditions and parameter values. We use the following syntax:
 
 # Define the initial conditions and parameters
 u0 = [u => 1.0]
 p = [a => 2.0]
 tspan = (0.0 , 5.0)
+
+# Finally, using the functions `ODEProblem` and `solve` from `DifferentialEquations` we convert our system to a numerical problem and solve it:
 
 # Set up the problem
 problem = ODEProblem(complete(system), u0, tspan, p)
@@ -79,10 +115,14 @@ problem = ODEProblem(complete(system), u0, tspan, p)
 # Solve the problem
 solution = solve(problem)
 
+# Now, we need to extract the time points and `u` values from the solution. To do this, we simply do:
+
 times = solution.t
 
 # The 1 here means we're extracting the values from the first variable 
 values = sol[1, :]  
+
+# Finally, we can plot the graph of this solution using what we've learned:
 
 using CairoMakie
 
@@ -95,5 +135,4 @@ axislegend(ax)
 
 display(fig)
 
-# ---------------------------------------------- #
-
+# Great! You just learned how to model your own ODE in Julia!
