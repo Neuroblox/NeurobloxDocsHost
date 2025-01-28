02c53684c3ed6059b08af3ee3a916c53cef717ef
Documenter.jl <documenter@juliadocs.github.io>
2025-01-28 17:07:22 +0000

    build based on 991f24c

--- dev/tutorials/spectralDCM.jl
+++ dev/tutorials/spectralDCM.jl
@@ -68,14 +68,14 @@ for idx in CartesianIndices(A_true)
 end
 
 # finally we compose the simulation model
-@named simmodel = system_from_graph(g, split=false)
+@named simmodel = system_from_graph(g)
 
 # ## Run the simulation and plot the results
 
 # setup simulation of the model, time in seconds
 tspan = (0.0, 512.0)
 prob = SDEProblem(simmodel, [], tspan)
-dt = 2   # 2 seconds (units are milliseconds) as measurement interval for fMRI
+dt = 2   # 2 seconds (units are seconds) as measurement interval for fMRI
 sol = solve(prob, ImplicitRKMil(), saveat=dt);
 
 # we now want to extract all the variables in our model which carry the tag "measurement". For this purpose we can use the Neuroblox function `get_idx_tagged_vars`
@@ -170,8 +170,10 @@ fitmodel = structural_simplify(fitmodel, split=false)   # and now simplify the e
 max_iter = 128;            # maximum number of iterations
 ## attribute initial conditions to states
 sts, _ = get_dynamic_states(fitmodel);
+sts = unknowns(fitmodel)
+rv = rand(length(sts))
 # the following step is needed if the model's Jacobian would give degenerate eigenvalues when expanded around the fixed point 0 (which is the default expansion). We simply add small random values to avoid this degeneracy:
-perturbedfp = Dict(sts .=> abs.(0.001*rand(length(sts))))     # slight noise to avoid issues with Automatic Differentiation.
+perturbedfp = OrderedDict(sts .=> abs.(0.001*rv[1:length(sts)]))     # slight noise to avoid issues with Automatic Differentiation.
 # For convenience we can use the default prior function to use standardized prior values as given in SPM:
 pmean, pcovariance, indices = defaultprior(fitmodel, nr)
 
@@ -179,9 +181,9 @@ priors = (μθ_pr = pmean,
           Σθ_pr = pcovariance
          );
 # Setup hyper parameter prior as well:
-hyperpriors = Dict(:Πλ_pr => 128.0*ones(1, 1),   # prior metaparameter precision, needs to be a matrix
-                   :μλ_pr => [8.0]               # prior metaparameter mean, needs to be a vector
-                  );
+hyperpriors = (Πλ_pr = 128.0*ones(1, 1),   # prior metaparameter precision, needs to be a matrix
+               μλ_pr = [8.0]               # prior metaparameter mean, needs to be a vector
+              );
 # To compute the cross spectral densities we need to provide the sampling interval of the time series, the frequency axis and the order of the multivariate autoregressive model:
 csdsetup = (mar_order = p, freq = freq, dt = dt);
 # earlier we used the function `get_idx_tagged_vars` to get the indices of tagged variables. Here we don't want to get the indices but rather the symbolic variable names themselves.
@@ -190,23 +192,17 @@ _, s_bold = get_eqidx_tagged_vars(fitmodel, "measurement");    # get bold signal
 # Prepare the DCM. This function will setup the computation of the Dynamic Causal Model. The last parameter specifies that wer are using fMRI time series as opposed to LFPs.
 (state, setup) = setup_sDCM(dfsol[:, String.(Symbol.(s_bold))], fitmodel, perturbedfp, csdsetup, priors, hyperpriors, indices, pmean, "fMRI");
 
-## HACK: on machines with very small amounts of RAM, Julia can run out of stack space while compiling the code called in this loop
-## this should be rewritten to abuse the compiler less, but for now, an easy solution is just to run it with more allocated stack space.
-with_stack(f, n) = fetch(schedule(Task(f, n)));
-
 # We are now ready to run the optimization procedure! :)
 # That is we loop over run_sDCM_iteration! which will alter `state` after each optimization iteration. It essentially computes the Variational Laplace estimation of expectation and variance of the tunable parameters. 
-with_stack(5_000_000) do  # 5MB of stack space
-    for iter in 1:max_iter
-        state.iter = iter
-        run_sDCM_iteration!(state, setup)
-        print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
-        if iter >= 4
-            criterion = state.dF[end-3:end] .< setup.tolerance
-            if all(criterion)
-                print("convergence\n")
-                break
-            end
+for iter in 1:max_iter
+    state.iter = iter
+    run_sDCM_iteration!(state, setup)
+    print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
+    if iter >= 4
+        criterion = state.dF[end-3:end] .< setup.tolerance
+        if all(criterion)
+            print("convergence\n")
+            break
         end
     end
 end
