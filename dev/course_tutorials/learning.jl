3ad081b9e4ce5d17fa691e94e0fc230a32f11b71
Documenter.jl <documenter@juliadocs.github.io>
2025-10-14 00:59:35 +0000

    build based on ad412dc

--- dev/course_tutorials/learning.jl
+++ dev/course_tutorials/learning.jl
@@ -128,10 +128,10 @@ adjacency(fig[1,1], agent; title = "Before Learning", colorrange=(0,0.2))
 
 trace = run_experiment!(agent, env; t_warmup=200.0, alg=Vern7(), verbose=true)
 
-# `trace` is a `NamedTuple` containing useful outcomes for each trial of the experiment: 
-trace.trial ## trial indices
-trace.correct ## whether the response was correct or not on each trial
-trace.action; ## what responce was made on each trial, 1 is left and 2 is right
+# `trace` is a vector of `NamedTuple`s containing useful outcomes for each trial of the experiment: 
+[x.trial for x in trace] ## trial indices
+[x.iscorrect for x in trace] ## whether the response was correct or not on each trial
+[x.action for x in trace]; ## what responce was made on each trial, 1 is left and 2 is right
 
 adjacency(fig[1,2], agent; title = "After Learning", colorrange=(0,0.2))
 fig
